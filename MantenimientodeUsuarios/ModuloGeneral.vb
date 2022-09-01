981671decea8b67c5744a731fa67f6088c876430
Kevin Medina <96708929+M1573R10@users.noreply.github.com>
2022-09-01 12:52:42 -0500

    Update ModuloGeneral.vb

--- MantenimientodeUsuarios/ModuloGeneral.vb
+++ MantenimientodeUsuarios/ModuloGeneral.vb
@@ -1,6 +1,6 @@
 ﻿Imports System.Data
 Imports System.Data.SqlClient
 Module ModuloGeneral
-    Public cadena As String = "Data Source=DESKTOP-UQP3LNL;Initial Catalog=MantenimientoUsuarios;Integrated Security=True"
+    Public cadena As String = "Data Source=ORDENADOR;Initial Catalog=MantenimientoUsuarios;Integrated Security=True"
     Public conexion As New SqlConnection(cadena)
 End Module
